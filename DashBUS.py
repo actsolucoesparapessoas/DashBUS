51efafcbe66e49cea177921f02ec6cb48deb0f45
actsolucoesparapessoas <129021588+actsolucoesparapessoas@users.noreply.github.com>
2024-07-04 19:03:26 -0300

    Update DashBUS.py

--- DashBUS.py
+++ DashBUS.py
@@ -3,5 +3,5 @@ from PIL import Image
 
 st.set_page_config(page_title='Em_Desenvolvimento', layout="centered", initial_sidebar_state="auto")
 image = Image.open('desenvolvimento.jpg')    
-st.image(image, width=800)
+st.image(image, width=600)
 st.title("Desenvolvedor: Massaki Igarashi")                 
